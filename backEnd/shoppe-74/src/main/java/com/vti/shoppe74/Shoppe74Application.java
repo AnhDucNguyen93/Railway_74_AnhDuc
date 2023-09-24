eaf04587672228dd8a417ba04a074d32a88fe0c7
AnhDucNguyen93 <nguyenanhduc.uct@gmail.com>
2023-09-24 22:41:51 +0700

    specification

---
+++ backEnd/shoppe-74/src/main/java/com/vti/shoppe74/Shoppe74Application.java
@@ -0,0 +1,13 @@
+package com.vti.shoppe74;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shoppe74Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shoppe74Application.class, args);
+    }
+
+}
